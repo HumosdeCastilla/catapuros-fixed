ff60ae2f50a6f3fc6ed54da15cefbba775bed0d3
HumosdeCastilla <jormugaray@gmail.com>
2025-04-30 13:33:29 +0200

    Initial commit

---
+++ vite.config.js
@@ -0,0 +1,34 @@
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+import { VitePWA } from 'vite-plugin-pwa';
+
+export default defineConfig({
+  plugins: [
+    react(),
+    VitePWA({
+      registerType: 'autoUpdate',
+      includeAssets: ['favicon.svg', 'icon-192.png', 'icon-512.png'],
+      manifest: {
+        name: 'CataPuros',
+        short_name: 'CataPuros',
+        description: 'App de cata de puros',
+        start_url: '/',
+        display: 'standalone',
+        theme_color: '#1e1e1e',
+        background_color: '#1e1e1e',
+        icons: [
+          {
+            src: '/icon-192.png',
+            sizes: '192x192',
+            type: 'image/png'
+          },
+          {
+            src: '/icon-512.png',
+            sizes: '512x512',
+            type: 'image/png'
+          }
+        ]
+      }
+    })
+  ]
+});
